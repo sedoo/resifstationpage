7ef2b32c66bc0232ef1e3ccedaecf1867b1c1f70
Francois ANDRE <francois.andre.prof@gmail.com>
2023-03-15 20:31:12 +0100

    Feat: Use WorkingFolder as parameter

--- majJour_1.0.0.py
+++ majJour_1.0.0.py
@@ -4,13 +4,20 @@ from obspy.clients.fdsn import Client
 from obspy.core import UTCDateTime
 import subprocess
 import os
+import sys
+
+workingFolder ='.'
+if len(sys.argv)>1:
+    workingFolder = sys.argv[1]
+   
+print("Working folder: "+workingFolder)
 
 deltaT = 86500
 t0 = UTCDateTime()
 t1 = t0 - deltaT
 
 # verification de l'arborescence des répertoires et de la présence des fichiers
-os.makedirs("./json", exist_ok=True)
+os.makedirs(workingFolder+"/json", exist_ok=True)
 
 client = Client("RESIF")
 liste_Stations = client.get_stations(
@@ -28,7 +35,7 @@ for sta in liste_Stations[0]:
     tday0 = UTCDateTime(t0.year, t0.month, t0.day-1)
     tday1 = UTCDateTime(t0.year, t0.month, t0.day)
     try:
-        ficout = "./jpg/%s_journalier.png" % sta.code
+        ficout = workingFolder+"/jpg/%s_journalier.png" % sta.code
         st = client.get_waveforms("FR", sta.code, "00", "HHZ", tday0, tday1)
         st.plot(type="dayplot", outfile=ficout)
         print("===> %s" % ficout)
@@ -38,8 +45,8 @@ for sta in liste_Stations[0]:
     ### creation des stats
     print("fichier json et histogramme de la station %s" % sta.code)
     try:
-        ficstat = "./jpg/%s_stats.jpg" % sta.code
-        ficjson = "./json/%s.json" % sta.code
+        ficstat = workingFolder+"/jpg/%s_stats.jpg" % sta.code
+        ficjson = workingFolder+"/json/%s.json" % sta.code
         subprocess.run(["./stat_ws_json.py", sta.code, "-b", ficstat, "-o", ficjson])
         print("===> %s" % ficstat)
         print("===> %s" % ficjson)
