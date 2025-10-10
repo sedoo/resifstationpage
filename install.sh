0214a967877c7abc5365856409fb70d80e97d705
Francois ANDRE <francois.andre.prof@gmail.com>
2025-10-10 16:19:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ fi
 python3 -m venv $PYENV
 . $PYENV/bin/activate
 python3 -m pip install --upgrade pip
+python3 -m pip install "numpy<2"
 python3 -m pip install matplotlib==3.7.1
 python3 -m pip install ObsPy==1.4.0
 python3 -m pip install docopt==0.6.2
